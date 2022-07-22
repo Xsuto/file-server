a81126743b006a5e0414199bd15eb80f81a3600e
Xsuto <michal.sutowicz@gmail.com>
2022-07-22 10:58:18 +0200

    test

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,15 +5,19 @@ import { AppService } from './app.service';
 import { MulterModule } from '@nestjs/platform-express';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileSchema } from './schemas/file.schema';
+console.log(process.env);
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }),
+    MongooseModule.forRoot(
+      process.env.DB_URI || 'mongodb://localhost:27017/files',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
     MongooseModule.forFeature([{ name: 'File', schema: FileSchema }]),
     MulterModule.register({
       dest: './uploads',
