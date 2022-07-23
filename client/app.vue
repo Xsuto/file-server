a6c46a8ebc85ae6feecd498e6f9612ad3d6db7ae
Xsuto <michal.sutowicz@gmail.com>
2022-07-23 04:21:11 +0200

    lint

--- client/app.vue
+++ client/app.vue
@@ -1,70 +1,85 @@
 <script lang="ts" setup>
-import FileSaver from 'file-saver'
-const URL = `http://${window.location.hostname}:9003`
-const uploadURL = `${URL}/upload`
-const fileURL = `${URL}/file/`
-const ytURL = `${URL}/yt/`
+import FileSaver from 'file-saver';
+const URL = `http://${window.location.hostname}:9003`;
+const uploadURL = `${URL}/upload`;
+const fileURL = `${URL}/file/`;
+const ytURL = `${URL}/yt/`;
 
 interface File {
-  ID: string
-  originalName: string
-  extension?: string
-  createdAt: Date
+  ID: string;
+  originalName: string;
+  extension?: string;
+  createdAt: Date;
 }
 
-const youtubeLink = $ref('')
-let files = $ref([])
-const { data: serverFiles, refresh } = $(await useFetch<File[]>(`${URL}/files`))
+const youtubeLink = $ref('');
+let files = $ref([]);
+const { data: serverFiles, refresh } = $(
+  await useFetch<File[]>(`${URL}/files`),
+);
 
-const formattedServerFiles = $computed(() => serverFiles.map(it =>
-  ({ ...it, createdAt: `${it.createdAt.toString().split('T')[0]} ${it.createdAt.toString().split('T')[1].split('.')[0]}` }),
-))
+const formattedServerFiles = $computed(() =>
+  serverFiles.map((it) => ({
+    ...it,
+    createdAt: `${it.createdAt.toString().split('T')[0]} ${
+      it.createdAt.toString().split('T')[1].split('.')[0]
+    }`,
+  })),
+);
 
 const uploadYoutubeVideo = async () => {
-  const url = ytURL + encodeURIComponent(youtubeLink)
-  await useFetch(url)
-  await refresh()
-}
+  const url = ytURL + encodeURIComponent(youtubeLink);
+  await useFetch(url);
+  await refresh();
+};
 
 const handleFormSubmit = async (event: Event) => {
-  const promises = []
+  const promises = [];
   for (const file of files) {
-    const formData = new FormData()
-    formData.append('file', file)
-    promises.push(fetch(uploadURL, {
-      method: 'POST',
-      body: formData,
-    }))
+    const formData = new FormData();
+    formData.append('file', file);
+    promises.push(
+      fetch(uploadURL, {
+        method: 'POST',
+        body: formData,
+      }),
+    );
   }
-  await Promise.all(promises)
-  await refresh()
-}
+  await Promise.all(promises);
+  await refresh();
+};
 
 const handleInputChange = (event) => {
-  files = event.target.files
-}
+  files = event.target.files;
+};
 
 const handleClick = (file: File) => {
-  FileSaver.saveAs(fileURL + file.ID, file.originalName + file?.extension)
-}
+  FileSaver.saveAs(fileURL + file.ID, file.originalName + file?.extension);
+};
 </script>
 
 <template>
   <div>
     <form @submit.prevent="uploadYoutubeVideo">
-      <input v-model="youtubeLink">
-      <button type="submit">
-        Get yt video
-      </button>
+      <input v-model="youtubeLink" />
+      <button type="submit">Get yt video</button>
     </form>
     <form @submit.prevent="handleFormSubmit($event)">
       <label for="file">File</label>
-      <input multiple name="file" required type="file" @change="handleInputChange($event)">
-      <button type="submit">
-        Upload
-      </button>
+      <input
+        multiple
+        name="file"
+        required
+        type="file"
+        @change="handleInputChange($event)"
+      />
+      <button type="submit">Upload</button>
     </form>
-    <p v-for="file in formattedServerFiles" :key="file" @click="handleClick(file)">
+    <p
+      v-for="file in formattedServerFiles"
+      :key="file"
+      @click="handleClick(file)"
+    >
       {{ file.originalName }} added at {{ file.createdAt }}
     </p>
   </div>
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,9 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    experimental: {
-        reactivityTransform: true
-    }
-})
+  ssr: false,
+  experimental: {
+    reactivityTransform: true,
+  },
+});
